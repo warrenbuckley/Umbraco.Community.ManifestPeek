23cecb69bb48fecf7b87ca78b3c6d2fd2a369d92
Warren Buckley <warren@hackmakedo.com>
2025-11-27 10:50:48 +0000

    Add build.ps1 & bump version to 17.0.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Manifest Peek
-This is a small utility package for Umbraco V14+ that allows you to view the backoffice manifests JSON.
+This is a small utility package for Umbraco V17+ that allows you to view the backoffice manifests JSON.
 
 Trying to understand how Umbraco backoffice extensions and other packages you install and what the shape of the JSON should be for the manifest and this is a tool in our toolkits to help with debugging and understandhing how extensions are registering and hopefully should save some source code diving into the Umbraco source code.
 
---
+++ build.ps1
@@ -0,0 +1,31 @@
+# Define the project file and configuration
+$projectFile = "./Umbraco.Community.ManifestPeek/Umbraco.Community.ManifestPeek.csproj"
+$configuration = "Release"
+$outputDirectory = "./build.out"
+$version = "17.0.0"
+$packageJsonPath = "./Umbraco.Community.ManifestPeek/wwwroot/umbraco-package.json"
+
+# Delete all files in the output directory
+if (Test-Path $outputDirectory) {
+    Remove-Item "$outputDirectory/*"
+}
+
+# Update the version in umbraco.package.json
+if (Test-Path $packageJsonPath) {
+    $packageJson = Get-Content $packageJsonPath | ConvertFrom-Json
+    $packageJson.version = $version
+    $packageJson | ConvertTo-Json -Depth 32 | Set-Content $packageJsonPath
+} else {
+    Write-Output "The file $packageJsonPath does not exist."
+}
+
+# Pack the project into a NuGet package
+dotnet pack $projectFile --configuration $configuration --output $outputDirectory /p:Version=$version
+
+# Check if the pack was successful
+if ($LASTEXITCODE -eq 0) {
+    Write-Output "Pack successful."
+} else {
+    Write-Output "Pack failed."
+    exit $LASTEXITCODE
+}
